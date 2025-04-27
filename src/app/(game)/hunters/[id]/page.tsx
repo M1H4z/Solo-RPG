d26843264cdf7209035fea6be1a2528730a05256
Muhammad Irfan Husaini Bin Amir <erfanhusaini@gmail.com>
2025-04-28 02:17:06 +0800

    fix: syntax errors

--- src/app/(game)/hunters/[id]/page.tsx
+++ src/app/(game)/hunters/[id]/page.tsx
@@ -1,33 +1,3 @@
-import React from "react";
-import { notFound, redirect } from "next/navigation";
-import { getHunterById } from "@/services/hunterService";
-import { getUserSession } from "@/services/authService";
-
-export default async function HunterDetailPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const { id: hunterId } = params;
-
-  // Check user session
-  const sessionData = await getUserSession();
-  if (!sessionData) {
-    redirect("/login");
-  }
-
-  // Fetch Hunter Data
-  const hunter = await getHunterById(hunterId);
-  if (!hunter) {
-    notFound();
-  }
-
-  return (
-    <div className="p-4">
-      <h1>Hunter Details</h1>
-      <p>Name: {hunter.name}</p>
-      <p>Level: {hunter.level}</p>
-      <p>Class: {hunter.class}</p>
-    </div>
-  );
+export default function HunterPage({ params }: { params: { id: string } }) {
+  return <div>Hunter ID: {params.id}</div>;
 }
